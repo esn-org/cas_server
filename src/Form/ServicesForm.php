ecdda88ebead1b43d5dbe58c34bbe5dfbd670b15
Tim Yale <yalet@lafayette.edu>
2016-05-31 14:03:14 -0400

    Build out service config entity form.

--- src/Form/ServicesForm.php
+++ src/Form/ServicesForm.php
@@ -39,6 +39,51 @@ class ServicesForm extends EntityForm {
     $service = $this->entity;
 
     // Form API stuff here
+    $form['label'] = array(
+      '#type' => 'textfield',
+      '#title' => $this->t('Label'),
+      '#maxlength' => 255,
+      '#default_value' => $service->getLabel(),
+      '#description' => $this->t('Label for the Service definition'),
+      '#required' => TRUE,
+    );
+
+    $form['id'] = array(
+      '#type' => 'machine_name',
+      '#default_value' => $service->getId(),
+      '#machine_name' => array( 
+        'exists' => array($this, 'exist'),
+      ),
+      '#disabled' => !$service->isNew(),
+    );
+
+    $form['service'] = array(
+      '#type' => 'textfield',
+      '#default_value' => $service->getService(),
+      '#title' => $this->t('Service URL Pattern'),
+      '#size' => 60,
+      '#maxlength' => 255,
+      '#required' => TRUE,
+      '#description' => $this->t('Pattern to match service urls with. * is a wildcard.'),
+    );
+
+    $form['sso'] = array(
+      '#type' => 'radios',
+      '#title' => $this->t('Participate in single sign on?'),
+      '#default_value' => $service->getSso(),
+      '#options' => array(0 => $this->t('No'), 1 => $this->t('Yes')),
+    );
+
+
+    $options = array_map(NULL, array_keys($this->entityFieldManager->getFieldDefinitions('user', 'user')));
+    $form['attributes'] = array(
+      '#type' => 'select',
+      '#title' => 'Released attributes',
+      '#description' => 'Fields to release as Cas attributes.',
+      '#multiple' => TRUE,
+      '#default_value' => $service->getAttributes(),
+      '#options' => $options,
+    );
 
     return $form;
   }
